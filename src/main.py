1642601355ac82602294c1cab733947302630237
gamboa9812-cyber <gamboa9812@gmail.com>
2026-05-12 12:34:46 -0500

    Se crea archivo principal con estructura base y manejo de errores

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Proyecto Fase 4 - Estabilidad, robustez y manejo de errores
+# Estudiante: Luz Dayanna Salazar Jaraba
+
+def main():
+    try:
+        print("Programa iniciado correctamente")
+
+        # Aquí se implementará la solución del ejercicio del Word
+
+    except Exception as e:
+        print("Error detectado:", e)
+
+if __name__ == "__main__":
+    main()
